a3280d3ff5a7b669b28728dbb9a38f3638d10854
Ashton Navarro <ash.navarro425@gmail.com>
2025-11-24 17:20:55 +1100

    Added ability to perform operations on two numbers only

--- calc.js
+++ calc.js
@@ -2,6 +2,7 @@
 let numOne;
 let numTwo;
 let operator;
+let result = 0;
 
 // Operations
 function add(a, b){
@@ -33,6 +34,12 @@ function operate(op, a, b){
     }
 }
 
+// Calculator logic
+function clearScreen(){
+    const screen = document.getElementById("screen");
+    screen.value = "0";
+}
+
 function selectNum(number){
     const screen = document.getElementById("screen");
     if(screen.value == 0){
@@ -41,7 +48,24 @@ function selectNum(number){
     screen.value += number;
 }
 
-function clearScreen(){
+function selectOp(op){
     const screen = document.getElementById("screen");
-    screen.value = "";
+    numOne = screen.value
+    if(containsOp(screen.value)) {
+        return;
+    } else {
+        screen.value += op
+        operator = op 
+    }
+}
+
+function containsOp(str) {
+    return ['+', '-', '*', '/'].some(op => str.includes(op));
+}
+
+function calculate() {
+    const screen = document.getElementById("screen");
+    numTwo = screen.value.split(operator).at(-1);
+    result = operate(operator, +numOne, +numTwo)
+    screen.value = result;
 }
